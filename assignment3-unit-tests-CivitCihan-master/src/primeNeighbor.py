169b0776bf31012d86c0f967736b052296bfc138
Cihan Çivit <162605338+CivitCihan@users.noreply.github.com>
2024-11-29 17:37:29 +0300

    Add files via upload

---
+++ assignment3-unit-tests-CivitCihan-master/src/primeNeighbor.py
@@ -0,0 +1,22 @@
+'''
+Created by Neda Topuz
+Modified by Cihan Çivit
+
+'''
+
+def findPrimeNumber(upperBound):
+    if upperBound < 2:
+        return False
+    for i in range(2, int(upperBound**0.5) + 1):
+        if upperBound % i == 0:
+            return False
+    return True
+
+def primeNeighbor(val):
+    if val < 1000:
+        for pri in range(int(val), 1, -1):
+            if findPrimeNumber(pri):
+                print(pri)
+                return pri
+        return 0 
+    else: return 0
---
+++ assignment3-unit-tests-CivitCihan-master/src/primeNeighborTest.py
@@ -0,0 +1,23 @@
+import unittest
+import primeNeighbor as pn
+
+class PrimeNeighborTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test010(self):
+        actualResult = pn.primeNeighbor(val='1001')
+        expectedResult = 19
+        self.assertEqual(expectedResult, actualResult)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
